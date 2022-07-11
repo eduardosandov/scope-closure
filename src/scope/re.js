a54521298912f6ed93e5093aa1cfe8d8342d96ea
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-07-10 22:43:09 -0500

    closure

--- src/scope/re.js
+++ src/scope/re.js
@@ -1,28 +1,22 @@
+
 var firstName; //undefined
 firstName = 'Eduardo';
 console.log(firstName);
 
 ////////////////////////
-
 var lastName = "Sandoval"; /// Declarar / asignar
 lastName = 'Juan';  // Reasignar
 console.log(lastName);
 
 ////////////////////////
-
 var seconName = 'sandof';  // Declarando / asignando
 var seconName = 'tech'; // Reasignamos
 console.log(seconName);
 
 ///////////////////////
-
-//let
-
 let fruit = 'Apple'; //declarar / asignar
 fruit = 'Kiwi'; //reasignar
-
 console.log(fruit);
-
 let fruit = 'Banana';
 console.log(fruit);
 
---
+++ src/scope/strict.js
@@ -0,0 +1,7 @@
+pi = 3.1416;
+console.log(pi);
+
+function myFunction() {
+    return pi = 3.1416;
+}
+console.log(myFunction());
