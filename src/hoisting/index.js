b2607869b0ce1817a9eabf603a68ecebc62f321b
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-07-10 23:02:30 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+//var nameOfDog;
+//console.log(nameOfDog);
+//var nameOfDog = 'Elmo';
+//console.log(nameOfDog);
+
+
+var elmo;
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`Elmejor perro es ${elmo}`);
+}
+
+var elmo = 'Elmito';
